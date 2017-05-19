71a8ea3ac05949a41a771a127e8c53ac50d23538
BlastCI <r.et.d@libre-informatique.fr>
2017-05-19 17:41:35 +0200

    DevKit updates

--- tests/Resources/bootstrap.php
+++ tests/Resources/bootstrap.php
@@ -1,15 +1,10 @@
 <?php
-
 /*
  * This file is part of the Blast Project package.
  *
- * Copyright (C) 2015-2017 Libre Informatique
- *
- * This file is licenced under the GNU GPL v3.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 // fix encoding issue while running text on different host with different locale configuration
 setlocale(LC_ALL, 'en_US.UTF-8');
 if (file_exists($file = __DIR__.'/autoload.php')) {
@@ -28,7 +23,7 @@ if ($files) {
     require_once current($files);
 }
 
-// try to get Blast for PHPunit
-if (file_exists($file = __DIR__.'/App/BlastTests.php')) {
+// try to get outer extension fake file
+if (file_exists($file = __DIR__.'/autoload_outer_extension.php')) {
     require_once $file;
 }
