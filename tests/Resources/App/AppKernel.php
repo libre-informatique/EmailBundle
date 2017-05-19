26a172f267a68ec410e48e6a5d64e8a6b9d5dd3a
François GERBES <francois.gerbes@libre-informatique.fr>
2017-05-19 11:53:28 +0200

    Default config_test.yml load

--- tests/Resources/App/AppKernel.php
+++ tests/Resources/App/AppKernel.php
@@ -50,7 +50,7 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config.yml');
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
 
     public function getCacheDir()
